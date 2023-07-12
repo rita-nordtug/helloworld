9fedcd0d0cfa8c0126ebc37ecf5c064b57c5a1a6
Rita Nordtug <rita.nordtug@mastercard.com>
2023-07-12 19:23:52 +0200

    initial version

---
+++ src/main/java/com/example/helloworld/Configuration.java
@@ -0,0 +1,8 @@
+package com.example.helloworld;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@org.springframework.context.annotation.Configuration
+@ComponentScan
+public class Configuration {
+}
---
+++ src/main/java/com/example/helloworld/Greeting.java
@@ -0,0 +1,3 @@
+package com.example.helloworld;
+
+public record Greeting(long id, String content) { }
